648cc403e285af45f6dc6fc94a361e8f6fcb6a73
MatiasNuniez <matiasnuniez192@gmail.com>
2024-02-21 03:24:49 -0300

    asdasd

--- src/components/sections/Base_de_datos.jsx
+++ src/components/sections/Base_de_datos.jsx
@@ -104,7 +104,9 @@ export const DDBB = () => {
               'user': user
             }
           })
+          .then(console.log(data), alert('Elementos ingresados correctamente'), setNombre(''), setNumero(''), setModelo(''), setLocal(''))
           setData([...data, res.data])
+          .catch(error => { alert('Error 403, no tiene permisos') })
         } else { alert('Complete todos los campos (Recuerde que numero lleva 10 numeros)') }
       } else if (op === 2) {
         const newData = {
