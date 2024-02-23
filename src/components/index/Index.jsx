8ed4970fa89f93a0c3adacd6aaa3d0982eb2e26a
MatiasNuniez <matiasnuniez192@gmail.com>
2024-02-23 05:20:21 -0300

    token

--- src/components/index/Index.jsx
+++ src/components/index/Index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import  imgPrestados  from '../../assets/img-celulares-prestados.jpg'
+import imgPrestados from '../../assets/img-celulares-prestados.jpg'
 import imgGeneral from '../../assets/img-datos-general.jpg'
 import imgRotos from '../../assets/rotos.jpg'
 import { useNavigate } from 'react-router-dom'
@@ -12,44 +12,43 @@ export const OptionsIndex = () => {
   useEffect(() => {
     setToken(localStorage.getItem('tokensantarosa30') || '')
   }, [])
-  
+
   return (
+    <div>
+      {token != '' ?
+        <div className='container'>
+          <div className='contentIndex'>
+            <a className='aCardIndex' href="/Base_de_datos">
+              <div className="card" style={{ width: '18rem' }}>
+                <img src={imgGeneral} className="card-img-top" style={{ height: '100%' }} alt="Base de datos" />
+                <div className="card-body">
+                  <p className="card-text">Base de datos</p>
+                </div>
+              </div>
+            </a>
 
-    <div className='container'>
-      { token != '' ? 
-      <div className='contentIndex'>
-      <a className='aCardIndex' href="/Base_de_datos">
-      <div className="card" style={{width:'18rem'}}>
-        <img src={imgGeneral} className="card-img-top" style={{height:'100%'}} alt="Base de datos"/>
-          <div className="card-body">
-            <p className="card-text">Base de datos</p>
+            <a className='aCardIndex' href="/Prestados">
+              <div className="card" style={{ width: '18rem' }}>
+                <img src={imgPrestados} className="card-img-top" style={{ height: '100%' }} alt="Prestados" />
+                <div className="card-body">
+                  <p className="card-text">Prestados</p>
+                </div>
+              </div>
+            </a>
+
+            <a className='aCardIndex' href="/Rotos">
+              <div className="card" style={{ width: '18rem' }}>
+                <img src={imgRotos} className="card-img-top" style={{ height: '100%' }} alt="Prestados" />
+                <div className="card-body">
+                  <p className="card-text">Rotos</p>
+                </div>
+              </div>
+            </a>
+
           </div>
-      </div>
-      </a>
-
-      <a className='aCardIndex' href="/Prestados">
-      <div className="card" style={{width:'18rem'}}>
-        <img src={imgPrestados} className="card-img-top" style={{height:'100%'}} alt="Prestados"/>
-          <div className="card-body">
-            <p className="card-text">Prestados</p>
-          </div>
-      </div>
-      </a>
-
-      <a className='aCardIndex' href="/Rotos">
-      <div className="card" style={{width:'18rem'}}>
-        <img src={imgRotos} className="card-img-top" style={{height:'100%'}} alt="Prestados"/>
-          <div className="card-body">
-            <p className="card-text">Rotos</p>
-          </div>
-      </div>
-      </a>
-
-      </div> 
-      : (navigate('/login'), alert('Inicie sesion'))
-
-}
-
+        </div>
+        : (navigate('/login'), alert('Inicie sesion'))
+      }
     </div>
 
   )
