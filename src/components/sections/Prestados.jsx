34deb6e706f436c0cbc1c90af5cb5b7ec0b56ca9
MatiasNuniez <matiasnuniez192@gmail.com>
2024-02-23 02:47:39 -0300

    delete

--- src/components/sections/Prestados.jsx
+++ src/components/sections/Prestados.jsx
@@ -219,21 +219,19 @@ export const Prestados = () => {
           </div>
 
 
-          <div className="modal" id="eliminarModal" tabindex={-1} role="dialog">
-            <div className="modal-dialog" role="document">
+          <div className="modal fade" id="eliminarModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
               <div className="modal-content">
                 <div className="modal-header">
-                  <h5 className="modal-title">Eliminar</h5>
-                  <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
+                  <h1 className="modal-title fs-5" id="exampleModalLabel">Eliminar</h1>
+                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div className="modal-body">
                   <p>Desea eliminar?</p>
                 </div>
                 <div className="modal-footer">
-                  <button type="button" className="btn btn-primary" onClick={() => deleteItem()}>Eliminar</button>
-                  <button type="button" className="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={() => setIdDelete('')}>Cerrar</button>
+                  <button type="button" className="btn btn-primary" onClick={() => deleteItem(idDelete)} >Eliminar</button>
                 </div>
               </div>
             </div>
