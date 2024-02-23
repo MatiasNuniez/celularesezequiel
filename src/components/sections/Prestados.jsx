2d8902868c89fdb556ae2563b292edaf257d1707
MatiasNuniez <matiasnuniez192@gmail.com>
2024-02-23 02:35:45 -0300

    asdasd

--- src/components/sections/Prestados.jsx
+++ src/components/sections/Prestados.jsx
@@ -189,7 +189,7 @@ export const Prestados = () => {
     <div>
       {token !== '' ?
         <div className="container">
-          <div className="modal fade" id="#exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div className="modal-dialog">
               <div className="modal-content">
                 <div className="modal-header">
