dc6abf02a33194cd43045dc990751d348dd809da
MatiasNuniez <matiasnuniez192@gmail.com>
2024-02-23 03:06:57 -0300

    delete

--- src/components/sections/Rotos.jsx
+++ src/components/sections/Rotos.jsx
@@ -199,20 +199,19 @@ export const Rotos = () => {
             </div>
           </div>
 
-          <div class="modal" id="eliminarModal" tabindex={-1} role="dialog">
-            <div class="modal-dialog" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
+          <div className="modal fade" id="eliminarModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h1 className="modal-title fs-5" id="exampleModalLabel">Eliminar</h1>
+                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
-                  <p>Desea eliminiar?</p>
+                <div className="modal-body">
+                  <p>Desea eliminar?</p>
                 </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-primary" onClick={() => deleteItem()}>Eliminar</button>
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal" onClick={() => setIdDelete('')}>Cerrar</button>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={() => setIdDelete('')}>Cerrar</button>
+                  <button type="button" className="btn btn-primary" onClick={() => deleteItem(idDelete)} >Eliminar</button>
                 </div>
               </div>
             </div>
