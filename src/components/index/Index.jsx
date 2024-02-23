949df73209ad1a17f16b693460d43516641b2b0e
MatiasNuniez <matiasnuniez192@gmail.com>
2024-02-23 05:28:51 -0300

    navigate

--- src/components/index/Index.jsx
+++ src/components/index/Index.jsx
@@ -48,7 +48,7 @@ export const OptionsIndex = () => {
 
           </div>
         </div>
-        : (navigate("/login"), alert("inicie sesion, dirijase a https://celularesezequiel.vercel.app/"))
+        : (navigate("/login"), alert("inicie sesion, dirijase a https://celularesezequiel.vercel.app/login"))
       }
     </div>
 
